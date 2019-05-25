944b63bc8e94fe0eb054dbb7fa121059a1f104cb
forilyam <For_ilya_m@mail.ru>
2019-05-25 22:16:26 +0500

    Добавлены решения заданий 7 и 8

--- src/test/java/ru/stqa/training/selenium/FindElementsTask.java
+++ src/test/java/ru/stqa/training/selenium/FindElementsTask.java
@@ -17,13 +17,6 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-/*
-Задание 7. Сделайте сценарий, проходящий по всем разделам админки
-Сделайте сценарий, который выполняет следующие действия в учебном приложении litecart.
-1) входит в панель администратора http://localhost/litecart/admin
-2) прокликивает последовательно все пункты меню слева, включая вложенные пункты
-3) для каждой страницы проверяет наличие заголовка
-*/
 public class FindElementsTask {
 
     private WebDriver driver;
